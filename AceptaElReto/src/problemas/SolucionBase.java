5961bb1ef61505be395a55710fbfcf11d62a239c
InigoGutierrez <inigogf.95@gmail.com>
2017-11-18 15:08:10 +0100

    Añadido esqueleto general de las soluciones.

---
+++ AceptaElReto/src/problemas/SolucionBase.java
@@ -0,0 +1,22 @@
+package problemas;
+
+public class SolucionBase {
+
+	static java.util.Scanner in;
+	  
+	  public static void main(String args[]) {
+	    in = new java.util.Scanner(System.in);
+	    
+	    int numCasos;
+	    numCasos = in.nextInt();
+	    for (int i = 0; i < numCasos; i++)
+	      casoDePrueba();
+	  }
+	  
+	  public static void casoDePrueba() {
+		    int n;
+		    n = in.nextInt();
+		    
+		    // Procesamiento de cada caso
+	  }
+}
