e8290ed3108c85d4dd9cf86c0c1bcb76d19436ef
InigoGutierrez <inigogf.95@gmail.com>
2017-11-18 15:22:49 +0100

    Actualizada solución base.

--- AceptaElReto/src/problemas/SolucionBase.java
+++ AceptaElReto/src/problemas/SolucionBase.java
@@ -3,20 +3,18 @@ package problemas;
 public class SolucionBase {
 
 	static java.util.Scanner in;
-	  
-	  public static void main(String args[]) {
-	    in = new java.util.Scanner(System.in);
-	    
-	    int numCasos;
-	    numCasos = in.nextInt();
-	    for (int i = 0; i < numCasos; i++)
-	      casoDePrueba();
-	  }
-	  
-	  public static void casoDePrueba() {
-		    int n;
-		    n = in.nextInt();
-		    
-		    // Procesamiento de cada caso
-	  }
+
+	public static void main(String args[]) {
+		in = new java.util.Scanner(System.in);
+		
+		int numCasos = in.nextInt();
+		for (int i = 0; i < numCasos; i++) {
+			casoDePrueba();
+		}
+	}
+	
+	public static void casoDePrueba() {
+		int n = in.nextInt();
+		// Procesamiento de cada caso
+	}
 }
