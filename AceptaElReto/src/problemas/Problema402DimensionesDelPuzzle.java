3f7848dc0abda4f164a964cc7ceeac40bd602016
InigoGutierrez <inigogf.95@gmail.com>
2017-12-02 17:52:54 +0100

    Problema 402 (Dimensiones del puzzle) subido, out of time exception.

---
+++ AceptaElReto/src/problemas/Problema402DimensionesDelPuzzle.java
@@ -0,0 +1,31 @@
+package problemas;
+
+public class Problema402DimensionesDelPuzzle {
+
+	static java.util.Scanner in;
+
+	public static void main(String args[]) {
+		in = new java.util.Scanner(System.in);
+		int caso;
+		while ( true ) {
+			caso = in.nextInt();
+			if ( caso == 0 ) {
+				break;
+			}
+			System.out.println(casoDePrueba(caso));
+		}
+	}
+	
+	public static int casoDePrueba(int caso) {
+		int lado = (int) Math.sqrt(caso);
+		while ( caso % lado != 0 ) {
+			lado++;
+		}
+		return lado;
+	}
+	
+	/*public static int findLowestNextDivisorInRange( int lado, int caso ) {
+		
+	}*/
+	
+}
